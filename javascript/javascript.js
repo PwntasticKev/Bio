601ea1832f18fb151961a49e2896927866a11297
Kevin Lee <pwntastickevin@hotmail.com>
2017-12-03 02:08:10 -0700

    massive updates

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,14 @@
-var shape = document.querySelector('#outer');
-var shapeLength = shape.getTotalLength();
-console.log(shapeLength);
+
+  function initMap() {
+    var myPos = {lat: 40.286189, lng: -111.7231454};
+    var img = new google.maps.MarkerImage('https://www.autosphere.ca/collisionmanagement/files/2014/02/google-maps-marker-web.png', null, null, null, new google.maps.Size(50,42));
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: myPos
+    });
+    var marker = new google.maps.Marker({
+      position: myPos,
+      map: map,
+      icon: img
+    });
+  }
